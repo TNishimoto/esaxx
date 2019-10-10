5ee98acd16a2ccd24cc282a7e764c9fa743ed765
Nishimoto <a32b16c4@gmail.com>
2019-10-10 14:26:19 +0900

    minor fix

--- src/main/common.hpp
+++ src/main/common.hpp
@@ -116,7 +116,7 @@ void writeText(std::string filename, std::vector<stool::LCPInterval<INDEX>> &int
 template <typename CHAR>
 void printText(std::vector<CHAR> &text){
     for(uint64_t i=0;i<text.size();i++){
-        std::cout << ( text[i] == 0 ? '$' : text[i]);        
+        std::cout << ( text[i] == 0 ? '$' : (char)text[i]);        
     }
     std::cout << std::endl;
 }
