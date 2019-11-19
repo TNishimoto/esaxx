bad5c9141780daefa06f69d6f33243c3183b931a
Nishimoto <a32b16c4@gmail.com>
2019-11-19 12:06:53 +0900

    script

--- scripts/ms.sh
+++ scripts/ms.sh
@@ -7,10 +7,13 @@ filearr=( "fib41" "rs.13" "tm29" "dblp.xml.00001.1" "dblp.xml.00001.2" "dblp.xml
 for file in ${filearr[@]}; do
 	echo "esaxx ms(esaxx) ${file}"
     nohup /usr/bin/time -f "#esaxx ms(esaxx) ${file}, %e time, %M KB" ~/esaxx/build/enumMaximalSubstring.out -i ${folderpath}/${file} -o ${outputpath}/${file}.msi2 -p 0 -m esaxx >> /mnt/server/nishimoto/result/esaxx_ms_output.log
-	echo "esaxx ms(sdsl) ${file}"
-    nohup /usr/bin/time -f "#esaxx ms(sdsl) ${file}, %e time, %M KB" ~/esaxx/build/enumMaximalSubstring.out -i ${folderpath}/${file} -o ${outputpath}/${file}.sdsl.msi2 -p 0 -m sdsl >> /mnt/server/nishimoto/result/esaxx_ms_sdsl_output.log
 	echo "esaxx ms(rlbwt) ${file}"
     nohup /usr/bin/time -f "#esaxx ms(rlbwt) ${file}, %e time, %M KB" ~/esaxx/build/enumMaximalSubstring.out -i ${folderpath}/${file} -o ${outputpath}/${file}.rlbwt.msi2 -p 0 -m rlbwt >> /mnt/server/nishimoto/result/esaxx_ms_rlbwt_output.log
-
  done
  echo "Finished."
+for file in ${filearr[@]}; do
+	echo "esaxx ms(sdsl) ${file}"
+    nohup /usr/bin/time -f "#esaxx ms(sdsl) ${file}, %e time, %M KB" ~/esaxx/build/enumMaximalSubstring.out -i ${folderpath}/${file} -o ${outputpath}/${file}.sdsl.msi2 -p 0 -m sdsl -u 1 >> /mnt/server/nishimoto/result/esaxx_ms_sdsl_output.log
+ done
+ echo "Finished."
+ 
