f6fcbf1478f9a968d0992db0dccaa5e1508c1b1c
nishimoto <a32b16c4@gmail.com>
2020-12-20 18:48:26 +0900

    ms

--- src/main/enumMaximalSubstring.cpp
+++ src/main/enumMaximalSubstring.cpp
@@ -8,16 +8,18 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include "stool/src/cmdline.h"
-#include "../test/old_postorder_maximal_substrings.hpp"
-#include "common.hpp"
-#include "libdivsufsort/sa.hpp"
-#include "../postorder_maximal_substring_intervals.hpp"
 #include <sdsl/suffix_arrays.hpp>
 #include <sdsl/lcp_dac.hpp>
 #include <sdsl/lcp_support_sada.hpp>
-#include "../../module/rlbwt_iterator/src/include/rlbwt_iterator.hpp"
+
+#include "stool/src/cmdline.h"
+#include "common.hpp"
+#include "libdivsufsort/sa.hpp"
+#include "../postorder_maximal_substring_intervals.hpp"
 #include "../forward_bwt.hpp"
+
+//#include "../test/old_postorder_maximal_substrings.hpp"
+//#include "../../module/rlbwt_iterator/src/include/rlbwt_iterator.hpp"
 using namespace std;
 using CHAR = char;
 using INDEX = uint64_t;
@@ -28,32 +30,35 @@ void iterateMSwithSDSL(){
 }
 */
 
-
-
 uint64_t input_text_size = 0;
 std::vector<std::pair<std::string, uint64_t>> execution_time_messages;
 
-std::vector<char> construct_bwt_using_sdsl(string filename, string text, bool usingMemory){
+std::vector<char> construct_bwt_using_sdsl(string filename, string text, bool usingMemory)
+{
   sdsl::csa_sada<> sa;
-  if(usingMemory){
+  if (usingMemory)
+  {
     construct_im(sa, text, 1);
-  }else{
+  }
+  else
+  {
     construct(sa, filename, 1);
   }
 
   std::vector<char> p;
   p.resize(sa.size(), 0);
-  for(uint64_t i=0;i<sa.bwt.size();i++){
+  for (uint64_t i = 0; i < sa.bwt.size(); i++)
+  {
     p[i] = sa.bwt[i];
   }
   return p;
-
 }
-uint64_t iterateMSWithSDSL(string filename, std::ofstream &out, bool usingMemory){
-
+uint64_t iterateMSWithSDSL(string filename, std::ofstream &out, bool usingMemory)
+{
 
   std::string text;
-  if(usingMemory){
+  if (usingMemory)
+  {
     std::string tmp = stool::load_string_from_file(filename, false);
     text.swap(tmp);
   }
@@ -77,9 +82,12 @@ uint64_t iterateMSWithSDSL(string filename, std::ofstream &out, bool usingMemory
 
   auto start_lcp = std::chrono::system_clock::now();
   sdsl::lcp_dac<> lcpArray;
-  if(usingMemory){
+  if (usingMemory)
+  {
     construct_im(lcpArray, text, 1);
-  }else{
+  }
+  else
+  {
     construct(lcpArray, filename, 1);
   }
   text.resize(0);
@@ -96,47 +104,48 @@ uint64_t iterateMSWithSDSL(string filename, std::ofstream &out, bool usingMemory
   uint64_t count = 0;
   for (auto it : pmsi)
   {
-	  out.write(reinterpret_cast<const char *>(&it), sizeof(stool::LCPInterval<INDEX>));
+    out.write(reinterpret_cast<const char *>(&it), sizeof(stool::LCPInterval<INDEX>));
     ++count;
   }
   auto end_ms = std::chrono::system_clock::now();
   double ms_construction_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_ms - start_ms).count();
   execution_time_messages.push_back(std::pair<std::string, uint64_t>("MS Construction time\t\t", ms_construction_time));
   return count;
-
 }
 
-
-
-uint64_t iterateMS(string filename, std::ofstream &out){
+uint64_t iterateMS(string filename, std::ofstream &out)
+{
   vector<CHAR> T = stool::load_char_vec_from_file(filename, true); // input text
   input_text_size = T.size();
 
+  // Construction Suffix Array
   auto start_sa = std::chrono::system_clock::now();
   std::vector<INDEX> sa = stool::construct_suffix_array(T);
   auto end_sa = std::chrono::system_clock::now();
   double sa_construction_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_sa - start_sa).count();
   execution_time_messages.push_back(std::pair<std::string, uint64_t>("SA construction time\t\t", sa_construction_time));
 
-
+  // Construction BWT
   using BWT = stool::esaxx::ForwardBWT<CHAR, std::vector<CHAR>, std::vector<INDEX>>;
-  BWT bwt(&T,&sa);
+  BWT bwt(&T, &sa);
 
+  // Construction LCP Array
   auto start_lcp = std::chrono::system_clock::now();
   std::vector<INDEX> lcpArray = stool::constructLCP<CHAR, INDEX>(T, sa);
   auto end_lcp = std::chrono::system_clock::now();
-
   double lcp_array_construction_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_lcp - start_lcp).count();
   execution_time_messages.push_back(std::pair<std::string, uint64_t>("LCP array construction time\t", lcp_array_construction_time));
 
+
+  // Enumerating maximal substring
   auto start_ms = std::chrono::system_clock::now();
-  stool::esaxx::PostorderMaximalSubstringIntervals<CHAR, INDEX, std::vector<INDEX>, BWT > pmsi;
+  stool::esaxx::PostorderMaximalSubstringIntervals<CHAR, INDEX, std::vector<INDEX>, BWT> pmsi;
   pmsi.construct(&lcpArray, &bwt);
 
-    uint64_t count = 0;
+  uint64_t count = 0;
   for (auto it : pmsi)
   {
-	  out.write(reinterpret_cast<const char *>(&it), sizeof(stool::LCPInterval<INDEX>));
+    out.write(reinterpret_cast<const char *>(&it), sizeof(stool::LCPInterval<INDEX>));
     ++count;
   }
   std::cout << "Peek stack size: " << pmsi.compute_peek_stack_size() << std::endl;
@@ -146,7 +155,6 @@ uint64_t iterateMS(string filename, std::ofstream &out){
   double ms_construction_time = std::chrono::duration_cast<std::chrono::milliseconds>(end_ms - start_ms).count();
   execution_time_messages.push_back(std::pair<std::string, uint64_t>("MS Construction time\t\t", ms_construction_time));
   return count;
-
 }
 
 int main(int argc, char *argv[])
@@ -165,7 +173,7 @@ int main(int argc, char *argv[])
   string outputFile = p.get<string>("output_file");
   //string format = p.get<string>("format");
   string format = "binary";
-  
+
   //string mode = p.get<string>("mode");
   //bool usingMemory = p.get<bool>("memory");
 
@@ -190,58 +198,26 @@ int main(int argc, char *argv[])
 
   auto start = std::chrono::system_clock::now();
 
-
-	std::ofstream out(outputFile, std::ios::out | std::ios::binary);
-	if (!out){
+  std::ofstream out(outputFile, std::ios::out | std::ios::binary);
+  if (!out)
+  {
     throw std::runtime_error("Cannot open the output file!");
   }
   uint64_t ms_count = 0;
   std::vector<stool::LCPInterval<INDEX>> intervals;
 
-    //mode = "non-compressed";
-    ms_count = iterateMS(inputFile, out);
-  /*
-  if(mode == "old"){
-    ms_count = iterateMSwithOldESAXX(inputFile, out);
-  }else if(mode == "sdsl"){
-    ms_count = iterateMSWithSDSL(inputFile, out, usingMemory);
-  }else{
-  }
-  */
+  //mode = "non-compressed";
+  ms_count = iterateMS(inputFile, out);
+
   auto end = std::chrono::system_clock::now();
   double elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
 
-  /*
-  if (isPrint)
-  {
-    vector<CHAR> T = stool::load_char_vec_from_file(inputFile, true);
-    std::vector<INDEX> sa = stool::construct_suffix_array(T);
-    std::cout << "Maximal substrings in the file" << std::endl;
-    stool::esaxx::print<CHAR, INDEX>(intervals, T, sa);
-  }
-  */
- /*
-  if (format != "binary")
-  {
-    std::cout << "csv" << std::endl;
-
-    vector<CHAR> T = stool::load_char_vec_from_file(inputFile, true);
-    std::vector<INDEX> sa = stool::construct_suffix_array(T);
-    stool::esaxx::writeText<CHAR, INDEX>(outputFile, intervals, T, sa);
-  }
-  else
-  {
-    stool::write_vector(outputFile, intervals, false);
-  }
-  */
-  
-
   std::cout << "\033[31m";
   std::cout << "______________________RESULT______________________" << std::endl;
-  //std::cout << "mode \t\t\t\t\t : " << mode << std::endl; 
+  //std::cout << "mode \t\t\t\t\t : " << mode << std::endl;
   std::cout << "File \t\t\t\t\t : " << inputFile << std::endl;
   std::cout << "Output \t\t\t\t\t : " << outputFile << std::endl;
-  std::cout << "Output format \t\t\t\t : " << format << std::endl;  
+  std::cout << "Output format \t\t\t\t : " << format << std::endl;
   std::cout << "The length of the input text \t\t : " << input_text_size << std::endl;
   //if(mode == "sdsl"){
   //  std::cout << "Using only main memory \t\t\t : " << (usingMemory ? "true" : "false") << std::endl;
@@ -249,11 +225,11 @@ int main(int argc, char *argv[])
   //std::cout << "The number of maximum substrings: " << maximumSubstringCount << std::endl;
   std::cout << "The number of maximum substrings \t : " << ms_count << std::endl;
   std::cout << "Excecution time \t\t\t : " << elapsed << "[ms]" << std::endl;
-  for(auto it : execution_time_messages){
-  std::cout << "|\t " << it.first << " : " << it.second << "[ms]" << std::endl;
-
+  for (auto it : execution_time_messages)
+  {
+    std::cout << "|\t " << it.first << " : " << it.second << "[ms]" << std::endl;
   }
-  
+
   std::cout << "_______________________________________________________" << std::endl;
   std::cout << "\033[39m" << std::endl;
 }
